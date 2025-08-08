7b89e927f67f2835bbbb0488cce537401f959342
ginzburg-dev <ginzburgdd@gmail.com>
2025-08-08 17:29:48 +0100

    Comment unit test

--- tests/test_gpt_chat_node.py
+++ tests/test_gpt_chat_node.py
@@ -1,12 +1,11 @@
 from aigen.nodes.gpt_chat_node import GPTChatNode
 from aigen.core.file_handler import FileHandler
 
-def test_gpt_chat_node_run():
-    yaml_data = FileHandler.read_yaml("././config/example_pipeline.yaml")
-    print(yaml_data)
-    node_cfg = yaml_data[0]
-    node = GPTChatNode(node_cfg['params'])
-    context = {}
-    node.run(context)
-    print(context)
-# read json cfg!!!
+#def test_gpt_chat_node_run():
+#    yaml_data = FileHandler.read_yaml("././config/example_pipeline.yaml")
+#    print(yaml_data)
+#    node_cfg = yaml_data[0]
+#    node = GPTChatNode(node_cfg['params'])
+#    context = {}
+#    node.run(context)
+#    print(context)
